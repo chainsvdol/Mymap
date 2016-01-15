09c6eb20a5b876bb45786df0d9970bb264936c47
chainsvdol <chainsvdol@gmail.com>
2016-01-15 17:31:50 +0700

    change layout

--- app/src/main/java/th/go/dol/vong/rachain/mymapgps/MapsActivity.java
+++ app/src/main/java/th/go/dol/vong/rachain/mymapgps/MapsActivity.java
@@ -1,5 +1,6 @@
 package th.go.dol.vong.rachain.mymapgps;
 
+import android.renderscript.Double3;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -7,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -14,29 +16,81 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private String centerLatString,centerLngString;
+    private LatLng centerLatLng;
+     private Double [] myLatDoubles={13.66927731,13.66604555,13.66531579,13.66856841};
+    private Double [] myLngDoubles={100.62328577,100.61918736,100.6239295,100.6274271};
+    private LatLng[] myLatLngs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.my_maps);
 
+        // Create LatLng
+        createLatLng();
 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.fragment);
           mapFragment.getMapAsync(this);
 
     } // Main
+
+    private void createLatLng() {
+          //Forcenter
+
+          centerLatString=getIntent().getStringExtra("Lat");
+          centerLngString=getIntent().getStringExtra("Lng");
+
+        Double latDouble =Double.parseDouble(centerLatString);
+        Double lngDouble =Double.parseDouble(centerLngString);
+
+        centerLatLng =new LatLng(latDouble,lngDouble);
+          // For Other Marker
+        myLatLngs =new LatLng[myLatDoubles.length];
+
+        for (int i=0;i<myLatDoubles.length;i++){
+
+            myLatLngs[i]=new LatLng(myLatDoubles[i],myLngDoubles[i]);
+
+        }  // For
+
+
+    }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerLatLng,16));
+        //mMap.addMarker(new MarkerOptions().position(centerLatLng)); // Default Marker
+        mMap.addMarker(new MarkerOptions()
+                .position(centerLatLng)
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.friend))); //
+        mMap.addMarker(new MarkerOptions()
+            .position(myLatLngs[0])
+            .icon(BitmapDescriptorFactory
+                       .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(myLatLngs[1])
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mMap.addMarker(new MarkerOptions()
+                .position(myLatLngs[2])
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+        mMap.addMarker(new MarkerOptions()
+                .position(myLatLngs[3])
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_BLUE )));
+
+
+
     }   // OnMapReady
 }  // Main class

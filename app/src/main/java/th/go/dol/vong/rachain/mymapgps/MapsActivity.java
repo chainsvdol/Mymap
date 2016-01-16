8dc4ba11e588ec24c7bc126c34483fac18bda318
chainsvdol <chainsvdol@gmail.com>
2016-01-16 15:19:03 +0700

    change layout

--- app/src/main/java/th/go/dol/vong/rachain/mymapgps/MapsActivity.java
+++ app/src/main/java/th/go/dol/vong/rachain/mymapgps/MapsActivity.java
@@ -1,8 +1,11 @@
 package th.go.dol.vong.rachain.mymapgps;
 
+import android.graphics.Color;
 import android.renderscript.Double3;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,8 +14,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
     private String centerLatString,centerLngString;
@@ -20,12 +26,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      private Double [] myLatDoubles={13.66927731,13.66604555,13.66531579,13.66856841};
     private Double [] myLngDoubles={100.62328577,100.61918736,100.6239295,100.6274271};
     private LatLng[] myLatLngs;
+    private Button normalButton,satelliteButton,terrainButton,hybidButton;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_maps);
+
+        // bind
+
+        bindwidget();
 
         // Create LatLng
         createLatLng();
@@ -37,6 +49,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
           mapFragment.getMapAsync(this);
 
     } // Main
+
+    private void bindwidget() {
+
+        normalButton= (Button) findViewById(R.id.button2);
+        satelliteButton= (Button) findViewById(R.id.button3);
+        terrainButton = (Button) findViewById(R.id.button4);
+        hybidButton = (Button) findViewById(R.id.button5);
+
+
+    }
 
     private void createLatLng() {
           //Forcenter
@@ -90,7 +112,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .icon(BitmapDescriptorFactory
                         .defaultMarker(BitmapDescriptorFactory.HUE_BLUE )));
 
+        changeTypeMap();
+
+        createPolyline();
+
+
 
 
     }   // OnMapReady
+
+    private void createPolyline() {
+
+        PolylineOptions objPolylineOptions =new PolylineOptions()
+                .add(myLatLngs[0])
+                .add(myLatLngs[1])
+                .add(myLatLngs[2])
+                .add(myLatLngs[3])
+                .add(myLatLngs[0])
+                .width(5)
+                .color(Color.BLUE);
+        mMap.addPolyline(objPolylineOptions);
+
+
+
+    }
+
+    private void changeTypeMap() {
+
+        normalButton.setOnClickListener(this);
+        satelliteButton.setOnClickListener(this);
+        terrainButton.setOnClickListener(this);
+        hybidButton.setOnClickListener(this);
+
+
+
+
+
+    }//   change type map
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.button2:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.button3:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.button4:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.button5:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+        }
+    }
 }  // Main class
